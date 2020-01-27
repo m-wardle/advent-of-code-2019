c241130ae4075e806cacdb5aa9be7958b7c659d1
Matt Wardle <mwardle.22@gmail.com>
2020-01-26 21:33:15 -0500

    Add solution for d3p2

--- d3/solution.js
+++ d3/solution.js
@@ -7,29 +7,34 @@ const wire1 = data[0].split(",").map(x => [x[0], parseInt(x.slice(1))])
 const wire2 = data[1].split(",").map(x => [x[0], parseInt(x.slice(1))])
 
 const mapWirePath = function (wire) {
-  let xDistance = 0
-  let yDistance = 0
-  let history = new Set()
+  let xDistance = 0;
+  let yDistance = 0;
+  let counter = 0;
+  let history = {};
 
   wire.forEach(dir => {
     if (dir[0] === "U") {
       for (let i = 1; i <= dir[1]; i++) {
-        history.add(`X${xDistance}Y${yDistance + i}`);
+        counter++
+        history[`X${xDistance}Y${yDistance + i}`] = history[`X${xDistance}Y${yDistance + i}`] || counter;
       }
       yDistance += dir[1]
     } else if (dir[0] === "D") {
       for (let i = 1; i <= dir[1]; i++) {
-        history.add(`X${xDistance}Y${yDistance - i}`);
+        counter++
+        history[`X${xDistance}Y${yDistance - i}`] = history[`X${xDistance}Y${yDistance - i}`] || counter;
       }
       yDistance -= dir[1]
     } else if (dir[0] === "R") {
       for (let i = 1; i <= dir[1]; i++) {
-        history.add(`X${xDistance + i}Y${yDistance}`);
+        counter++
+        history[`X${xDistance + i}Y${yDistance}`] = history[`X${xDistance + i}Y${yDistance}`] || counter;
       }
       xDistance += dir[1]
     } else if (dir[0] === "L") {
       for (let i = 1; i <= dir[1]; i++) {
-        history.add(`X${xDistance - i}Y${yDistance}`);
+        counter++
+        history[`X${xDistance - i}Y${yDistance}`] = history[`X${xDistance - i}Y${yDistance}`] || counter;
       }
       xDistance -= dir[1]
     }
@@ -42,44 +47,50 @@ const checkWirePath = function (wire, history) {
   let xDistance = 0;
   let yDistance = 0;
   let matches = [];
+  let counter = 0;
 
   wire.forEach(dir => {
     if (dir[0] === "U") {
       for (let i = 1; i <= dir[1]; i++) {
-        if (history.has(`X${xDistance}Y${yDistance + i}`)) {
-          matches.push(Math.abs(xDistance) + Math.abs(yDistance + i))
+        counter++;
+        if (history[`X${xDistance}Y${yDistance + i}`]) {
+          matches.push([Math.abs(xDistance) + Math.abs(yDistance + i), history[`X${xDistance}Y${yDistance + i}`], counter])
         };
       }
       yDistance += dir[1]
     } else if (dir[0] === "D") {
       for (let i = 1; i <= dir[1]; i++) {
-        if (history.has(`X${xDistance}Y${yDistance - i}`)) {
-          matches.push(Math.abs(xDistance) + Math.abs(yDistance - i))
+        counter++;
+        if (history[`X${xDistance}Y${yDistance - i}`]) {
+          matches.push([Math.abs(xDistance) + Math.abs(yDistance - i), history[`X${xDistance}Y${yDistance - i}`], counter])
         };
       }
       yDistance -= dir[1]
     } else if (dir[0] === "R") {
       for (let i = 1; i <= dir[1]; i++) {
-        if (history.has(`X${xDistance + i}Y${yDistance}`)) {
-          matches.push(Math.abs(xDistance + i) + Math.abs(yDistance))
+        counter++;
+        if (history[`X${xDistance + i}Y${yDistance}`]) {
+          matches.push([Math.abs(xDistance + i) + Math.abs(yDistance), history[`X${xDistance + i}Y${yDistance}`], counter])
         };
       }
       xDistance += dir[1]
     } else if (dir[0] === "L") {
       for (let i = 1; i <= dir[1]; i++) {
-        if (history.has(`X${xDistance - i}Y${yDistance}`)) {
-          matches.push(Math.abs(xDistance - i) + Math.abs(yDistance))
+        counter++;
+        if (history[`X${xDistance - i}Y${yDistance}`]) {
+          matches.push([Math.abs(xDistance - i) + Math.abs(yDistance), history[`X${xDistance - i}Y${yDistance}`], counter])
         };
       }
       xDistance -= dir[1]
     }
   })
 
-  let min = matches[0];
+  let min = (matches[0][1] + matches[0][2]);
 
   matches.forEach(x => {
-    if (x < min) {
-      min = x;
+    let steps = x[1] + x[2]
+    if (steps < min) {
+      min = steps;
     }
   })
 
@@ -88,9 +99,5 @@ const checkWirePath = function (wire, history) {
 }
 
 const wire1Path = mapWirePath(wire1);
-// console.log(wire1Path)
-
-
-
 
 console.log(checkWirePath(wire2, wire1Path))
